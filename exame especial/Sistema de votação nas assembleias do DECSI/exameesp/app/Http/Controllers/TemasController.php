f1624c3f7f65cdea2514c1950d62b510bc1edfc7
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2022-06-22 18:11:51 -0300

    Add files via upload

---
+++ exame especial/Sistema de votação nas assembleias do DECSI/exameesp/app/Http/Controllers/TemasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\temas;
+use App\Http\Requests\StoretemasRequest;
+use App\Http\Requests\UpdatetemasRequest;
+
+class TemasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoretemasRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoretemasRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\temas  $temas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(temas $temas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\temas  $temas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(temas $temas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatetemasRequest  $request
+     * @param  \App\Models\temas  $temas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatetemasRequest $request, temas $temas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\temas  $temas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(temas $temas)
+    {
+        //
+    }
+}
---
+++ exame especial/Sistema de votação nas assembleias do DECSI/exameesp/app/Http/Controllers/VotosController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\votos;
+use App\Http\Requests\StorevotosRequest;
+use App\Http\Requests\UpdatevotosRequest;
+
+class VotosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorevotosRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorevotosRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\votos  $votos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(votos $votos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\votos  $votos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(votos $votos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatevotosRequest  $request
+     * @param  \App\Models\votos  $votos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatevotosRequest $request, votos $votos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\votos  $votos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(votos $votos)
+    {
+        //
+    }
+}
