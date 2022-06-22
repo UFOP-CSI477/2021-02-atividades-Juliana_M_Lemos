e6e96853eeac11f5c0e121bf7eacb4847b2e8483
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2022-06-22 16:48:29 -0300

    Add files via upload

---
+++ exame especial/Sistema de votação nas assembleias do DECSI/exameesp/database/migrations/2022_06_22_144424_create_votos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votos', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('user_id');
+            $table->integer('tema_id');
+            $table->tinyinteger('opcao');
+            $table->dateTime('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votos');
+    }
+};
